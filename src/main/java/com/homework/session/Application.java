f1b4cc5523ce438febd8124fbd0fc5285704c66c
cheomuk <pkw9815@naver.com>
2022-09-13 17:28:54 +0900

    Fix EC2 Server

--- src/main/java/com/homework/session/Application.java
+++ src/main/java/com/homework/session/Application.java
@@ -10,9 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class Application {
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:application-real.yml,"
-            + "classpath:application-s3.yml";
+            + "classpath:/application.yml,"
+            + "classpath:/application-real.yml";
 
     public static void main(String[] args){
         new SpringApplicationBuilder(Application.class).properties(APPLICATION_LOCATIONS).run(args);
