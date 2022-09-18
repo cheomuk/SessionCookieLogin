ee0a1c24b77b8b106830069109201b24f07547e3
cheomuk <pkw9815@naver.com>
2022-09-18 18:25:35 +0900

    Revise CheckUser

--- src/main/java/com/homework/session/service/LoginService.java
+++ src/main/java/com/homework/session/service/LoginService.java
@@ -63,7 +63,7 @@ public class LoginService {
             User user = userRepository.findByEmail(email).orElseThrow(() ->
                 { throw new UnAuthorizedException("E0002", ACCESS_DENIED_EXCEPTION); });
 
-            if (user.getIntroduction() == "자기를 소개해주세요.") {
+            if (user.getIntroduction() == "") {
                 userRepository.delete(user);
                 sessionCarrier.add("message", "회원가입이 제대로 되지 않았던 회원입니다.");
                 return sessionCarrier;
@@ -88,7 +88,7 @@ public class LoginService {
             User userDto = User.builder()
                     .nickname(nickname)
                     .email(email)
-                    .introduction("자기를 소개해주세요.")
+                    .introduction("")
                     .userRole(UserRole.USER)
                     .build();
 
